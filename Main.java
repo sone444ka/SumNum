d009482e86e66d2336335b93a7adaa54203353f1
sofia zolotaryova <sofiazolotaryova@MacBook-Air-4.local>
2026-01-19 16:18:16 +0100

    SumNum solution

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= 6; i++) {
+            sum += i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sum);
+        }
+
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
+    }
+}
+
+
+
+
+
+
+ls
+
+
